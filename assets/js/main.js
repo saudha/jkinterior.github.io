9bd1acac9ec2f43af770ae48170367af89bba86d
saudha shoeb <saudhashoeb02@gmail.com>
2020-07-06 08:34:11 -0400

    hvhgui
    hello

---
+++ assets/js/main.js
@@ -0,0 +1,59 @@
+//////swiper
+  //////////////////////
+  $(document).ready(function(){
+    // Add smooth scrolling to all links
+    $("a").on('click', function(event) {
+  
+      // Make sure this.hash has a value before overriding default behavior
+      if (this.hash !== "") {
+        // Prevent default anchor click behavior
+        event.preventDefault();
+  
+        // Store hash
+        var hash = this.hash;
+  
+        // Using jQuery's animate() method to add smooth page scroll
+        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 800, function(){
+     
+          // Add hash (#) to URL when done scrolling (default click behavior)
+          window.location.hash = hash;
+        });
+      } // End if
+    });
+  });
+
+///////b-t starts here
+
+  ////b-t ends here
+
+  /////animation
+
+  // All animations will take twice the time to accomplish
+document.documentElement.style.setProperty('--animate-duration', '2s');
+
+// All animations will take half the time to accomplish
+document.documentElement.style.setProperty('--animate-duration', '.5s');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
